b66275babcf9c111a2182f806a565bdcb355b0a7
pedro <pedrocivente@gmail.com>
2016-02-03 16:28:23 +0000

    new folder structure

---
+++ Logistics/feasibility/om.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+"""
+@author: WavEC Offshore Renewables
+email: boris.teillant@wavec.org; paulo@wavec.org
+
+This module is part of the characterization step in the WP5 methodology. It
+contains feasibility functions to compute the minimum logistic requirements to
+carry out the different logistic phases. This particular modules includes the
+function related to the O&M repair actions.
+
+BETA VERSION NOTES: The current version is limited to one logistic phase, the
+repair action related to offshore inspection and maintenance activities.
+"""
+
+def om_feas(log_phase, log_phase_id, OM_outputs, user_inputss):
+    """ om_feas is a function which determines the logistic requirement
+    associated with the logistic phases related to the O&M
+
+    Parameters
+    ----------
+    log_phase : Class
+     Class of the logistic phase under consideration for assessment
+    log_phase_id : str
+     string describing the ID of the logistic phase under consideration
+    wp6_outputs : dict
+     dictionnary containing all required inputs to WP5 coming from WP6
+
+    Returns
+    -------
+    feas_e : dict
+     dictionnary containing all logistic requirements associated with every
+     equipment type of the logistic phase under consideration
+    feas_v : dict
+     dictionnary containing all logistic requirements associated with every
+     vessel type of the logistic phase under consideration
+    """
+
+    ''' LogPhase LpM1: Inspection or on-site maintenance of topside elements'''
+
+    if log_phase_id == 'LpM1':
+
+        # Input collection
+        lenght_SP = OM_outputs['sp_length [m]']
+        width_SP = OM_outputs['sp_width [m]']
+        dry_mass_SP = OM_outputs['sp_dry_mass [kg]']/1000
+        nr_technician = OM_outputs['technician [-]']
+
+        # Feasibility functions
+        deck_area = max(lenght_SP*width_SP)
+        deck_cargo = max(dry_mass_SP)
+        deck_loading = max(dry_mass_SP/(lenght_SP*width_SP))
+        ext_personnel = max(nr_technician)
+
+        feas_e = {}
+        feas_v = {'CTV':        [['Deck loading [t/m^2]', 'sup', deck_loading],
+                                 ['Deck space [m^2]', 'sup', deck_area],
+                                 ['Max. cargo [t]', 'sup', deck_cargo],
+                                 ['External  personnel [-]', 'sup', ext_personnel]],
+
+                  'Multicat':   [['Deck loading [t/m^2]', 'sup', deck_loading],
+                                 ['Deck space [m^2]', 'sup', deck_area],
+                                 ['Max. cargo [t]', 'sup', deck_cargo],
+                                 ['External  personnel [-]', 'sup', ext_personnel]],
+
+                  'Helicopter': [['Deck loading [t/m^2]', 'sup', deck_loading],
+                                 ['Deck space [m^2]', 'sup', deck_area],
+                                 ['Max. cargo [t]', 'sup', deck_cargo],
+                                 ['External  personnel [-]', 'sup', ext_personnel]]
+
+                                 }
+
+        # Matching
+
+        feas_m_pv = {'CTV': [ ['Beam [m]', 'sup', 'Entrance width [m]'],
+                              ['Length [m]', 'sup', 'Terminal length [m]'],
+                              ['Max. draft [m]', 'sup', 'Terminal draught [m]'] ],
+
+                     'Multicat': [ ['Beam [m]', 'sup', 'Entrance width [m]'],
+                                   ['Length [m]', 'sup', 'Terminal length [m]'],
+                                   ['Max. draft [m]', 'sup', 'Terminal draught [m]'] ]
+                        }
+
+        feas_m_pe = {}
+
+        feas_m_ve = {}
+
+    if log_phase_id == 'LpM2':
+
+        # Input collection
+        lenght_SP = OM_outputs['sp_length [m]']
+        width_SP = OM_outputs['sp_width [m]']
+        dry_mass_SP = OM_outputs['sp_dry_mass [kg]']/1000
+        nr_technician = OM_outputs['technician [-]']
+        depth = OM_outputs['depth [m]']
+
+        # Feasibility functions
+        deck_area = max(lenght_SP*width_SP)
+        deck_cargo = max(dry_mass_SP)
+        deck_loading = max(dry_mass_SP/(lenght_SP*width_SP))
+        ext_personnel = max(nr_technician)
+        bathymetry = max(depth)
+
+        feas_e = {'divers': [ ['Max operating depth [m]', 'sup', bathymetry] ]
+
+                           }
+
+        feas_v = {'CTV':        [ ['Deck loading [t/m^2]', 'sup', deck_loading],
+                                  ['Deck space [m^2]', 'sup', deck_area],
+                                  ['Max. cargo [t]', 'sup', deck_cargo],
+                                  ['External  personnel [-]', 'sup', ext_personnel] ],
+
+                  'Multicat':   [ ['Deck loading [t/m^2]', 'sup', deck_loading],
+                                  ['Deck space [m^2]', 'sup', deck_area],
+                                  ['Max. cargo [t]', 'sup', deck_cargo],
+                                  ['External  personnel [-]', 'sup', ext_personnel] ]
+
+                                 }
+
+        # Matching
+
+        feas_m_pv = {'CTV': [ ['Beam [m]', 'sup', 'Entrance width [m]'],
+                              ['Length [m]', 'sup', 'Terminal length [m]'],
+                              ['Max. draft [m]', 'sup', 'Terminal draught [m]'] ],
+
+                     'Multicat': [ ['Beam [m]', 'sup', 'Entrance width [m]'],
+                                   ['Length [m]', 'sup', 'Terminal length [m]'],
+                                   ['Max. draft [m]', 'sup', 'Terminal draught [m]'] ]
+                        }
+
+        feas_m_pe = {}
+
+        feas_m_ve = {}
+
+    if log_phase_id == 'LpM3':
+
+        # Input collection
+        lenght_SP = OM_outputs['sp_length [m]']
+        width_SP = OM_outputs['sp_width [m]']
+        dry_mass_SP = OM_outputs['sp_dry_mass [kg]']/1000
+        nr_technician = OM_outputs['technician [-]']
+        depth = OM_outputs['depth [m]']
+        om_id = OM_outputs['id [-]'].ix[0]
+
+        # Feasibility functions
+        deck_area = max(lenght_SP*width_SP)
+        deck_cargo = max(dry_mass_SP)
+        deck_loading = max(dry_mass_SP/(lenght_SP*width_SP))
+        ext_personnel = max(nr_technician)
+        bathymetry = max(depth)
+
+        if om_id == 'Insp5':
+            feas_e = {'rov': [ ['Depth rating [m]', 'sup', bathymetry],
+                               ['ROV class [-]', 'equal', 'Workclass'] ]
+                           }
+        else:
+            feas_e = {'rov': [ ['Depth rating [m]', 'sup', bathymetry],
+                               ['ROV class [-]', 'equal', 'Inspection class'] ]
+                           }
+
+        feas_v = {'CTV':        [ ['Deck loading [t/m^2]', 'sup', deck_loading],
+                                  ['Deck space [m^2]', 'sup', deck_area],
+                                  ['Max. cargo [t]', 'sup', deck_cargo],
+                                  ['External  personnel [-]', 'sup', ext_personnel] ],
+
+                  'Multicat':   [ ['Deck loading [t/m^2]', 'sup', deck_loading],
+                                  ['Deck space [m^2]', 'sup', deck_area],
+                                  ['Max. cargo [t]', 'sup', deck_cargo],
+                                  ['External  personnel [-]', 'sup', ext_personnel] ]
+
+                                 }
+
+        # Matching
+
+        feas_m_pv = {'CTV': [ ['Beam [m]', 'sup', 'Entrance width [m]'],
+                              ['Length [m]', 'sup', 'Terminal length [m]'],
+                              ['Max. draft [m]', 'sup', 'Terminal draught [m]'] ],
+
+                     'Multicat': [ ['Beam [m]', 'sup', 'Entrance width [m]'],
+                                   ['Length [m]', 'sup', 'Terminal length [m]'],
+                                   ['Max. draft [m]', 'sup', 'Terminal draught [m]'] ]
+                        }
+
+        feas_m_pe = {}
+
+        feas_m_ve = {}
+
+    return feas_e, feas_v, feas_m_pv, feas_m_pe, feas_m_ve
+
---
+++ Operation_and_Maintenance/main_OM.py
@@ -0,0 +1,219 @@
+"""
+@author: WavEC Offshore Renewables
+email: boris.teillant@wavec.org; paulo@wavec.org
+
+main.py is the main file of the WP5 module within the suite of design tools
+developped under the EU FP7 DTOcean project. main.py provides an estimation of
+the predicted performance of feasible maritime infrastructure solutions
+that can carry out marine operations pertaining to the installation of
+wave and tidal energy arrays.
+
+main.py can be described in five core sub-modules:
+0- Loading input data
+1- Initialising the logistic classes
+2- Defining the installation plan
+3- Selecting the installation port
+4- Performing the assessment of all logistic phases sequencially, following
+   six steps:
+    (i) characterizartion of logistic requirements
+    (ii) selection of the maritime infrastructure
+    (iii) schedule assessment of the logistic phase
+    (iv) cost assessment of the logistic phase
+    (v) risk assessment of the logistic phase
+    (vi) environmental impact assessment of the logistic phase
+
+Parameters
+----------
+vessels(DataFrame): Panda table containing the vessel database
+
+equipments (DataFrame): Panda table containing the equipment database
+
+ports (DataFrame): Panda table containing the ports database
+
+user_inputs (dict): dictionnary containing all required inputs to WP5 coming from WP1/end-user:
+     'device' (Dataframe): inputs required from the device
+     'metocean' (Dataframe): metocean data
+
+hydrodynamic_outputs (dict): dictionnary containing all required inputs to WP5 coming from WP2
+     'units' (DataFrame): number of devices
+     'position' (DataFrame): UTM position of the devices
+
+electrical_outputs (dict): dictionnary containing all required inputs to WP5 coming from WP3
+     'layout' (DataFrame): to be specified
+
+M&F_outputs (DataFrame): containing foundation data required for each device
+
+O&M_outputs (dict):  dictionnary containing all required inputs to WP5 coming from WP6
+     'LogPhase1' (DataFrame): All inputs required for LpM1 logistic phase as defined by WP6
+
+Returns
+-------
+
+install (dict): dictionnary compiling all key results obtained from the assessment of the logistic phases for installation
+    'plan' (dict): installation sequence of the required logistic phases
+    'port' (DataFrame): port data related to the selected installation port
+    'requirement' (tuple): minimum requirements returned from the feasibility functions
+    'eq_select' (dict): list of equipments satisfying the minimum requirements
+    've_select' (dict): list of vessels satisfying the minimum requirements
+    'combi_select' (dict): list of solutions passing the compatibility check
+    'schedule' (dict): list of parameters with data about time
+    'cost'  (dict): vessel equiment and port cost
+    'risk': to be defined
+    'envir': to be defined
+    'status': to be defined
+
+
+Examples
+--------
+>>> LOGISTICS()
+
+
+See also: ...
+
+                       DTOcean project
+                    http://www.dtocean.eu
+
+                   WavEC Offshore Renewables
+                    http://www.wavec.org/en
+
+"""
+
+from os import path
+import os
+import sys
+sys.path.append('..')
+
+from Logistics.load import load_phase_order_data, load_time_olc_data
+from Logistics.load import load_eq_rates
+from Logistics.load import load_sf
+from Logistics.load import load_vessel_data, load_equipment_data
+from Logistics.load import load_port_data
+from Logistics.load.wp_bom import load_user_inputs, load_hydrodynamic_outputs
+from Logistics.load.wp_bom import load_electrical_outputs, load_MF_outputs
+from Logistics.load.wp_bom import load_OM_outputs
+
+from Logistics.phases.operations import logOp_init
+from Logistics.phases.om import logPhase_om_init
+from Logistics.installation import select_port_OM
+from Logistics.feasibility.glob import glob_feas
+from Logistics.selection.select_ve import select_e, select_v
+from Logistics.selection.match import compatibility_ve
+from Logistics.performance.schedule.schedule import sched
+from Logistics.performance.economic.eco import cost
+
+# # Set directory paths for loading inputs
+mod_path = path.dirname(path.realpath(__file__))
+
+def database_file(file):
+    """shortcut function to load files from the database folder
+    """
+    fpath = path.join('databases', '{0}'.format(file))
+    db_path = path.join(mod_path, fpath)
+    return db_path
+
+#def run():
+"""
+Load required inputs and database into panda dataframes
+"""
+
+import pickle
+
+# inputs_SV_LD = 'save'
+inputs_SV_LD = 'load'
+
+if inputs_SV_LD == "save":
+    # Saving the objects:
+
+    #default_values inputs
+    phase_order = load_phase_order_data(database_file("Installation_Order.xlsx"))
+    schedule_OLC = load_time_olc_data(database_file("operations_time_OLC.xlsx"))
+    penet_rates, laying_rates = load_eq_rates(database_file("equipment_perf_rates.xlsx"))
+    port_sf, vessel_sf, eq_sf = load_sf(database_file("safety_factors.xlsx"))
+
+    #Internal logistic module databases
+    vessels = load_vessel_data(database_file("logisticsDB_vessel_python.xlsx"))
+    equipments = load_equipment_data(database_file("logisticsDB_equipment_python.xlsx"))
+    ports = load_port_data(database_file("logisticsDB_ports_python.xlsx"))
+
+    #upstream module inputs/outputs
+    user_inputs = load_user_inputs(database_file("inputs_user.xlsx"))
+    hydrodynamic_outputs = load_hydrodynamic_outputs(database_file("ouputs_hydrodynamic.xlsx"))
+    electrical_outputs = load_electrical_outputs(database_file("ouputs_electrical.xlsx"))
+    MF_outputs = load_MF_outputs(database_file("outputs_MF.xlsx"))
+    OM_outputs = load_OM_outputs(database_file("outputs_OM.xlsx"))
+    OM_outputs_PORT = load_OM_outputs(database_file("outputs_OM_INS_PORT.xlsx"))
+
+    with open('objs.pickle', 'w') as f:
+        pickle.dump([phase_order, schedule_OLC, vessels, equipments, ports, user_inputs, hydrodynamic_outputs, electrical_outputs, MF_outputs, OM_outputs, OM_outputs_PORT], f)
+
+elif inputs_SV_LD == "load":
+    # Getting back the objects:
+    with open('objs.pickle') as f:
+        phase_order, schedule_OLC, vessels, equipments, ports, user_inputs, hydrodynamic_outputs, electrical_outputs, MF_outputs, OM_outputs, OM_outputs_PORT = pickle.load(f)
+
+else:
+    print 'Invalid SaveLoad option'
+
+"""
+ Initialise logistic operations and logistic phases
+"""
+
+# logOp = logOp_init()
+
+logOp = logOp_init(database_file("operations_time_OLC.xlsx"))
+
+
+
+
+"""
+Select the most appropriate base port for OM
+OM_port function selects the port used by OM logistic phases
+    required by the O&M module, depending if is inspection or actual maintenance.
+    For the case of inspection the closest port is chosen and the ID in the input should be INS_PORT.
+    For the case of the other logistic phases the selection is based on a 2 step process:
+        1 - the port feasibility functions from all logistic phases are taken
+        into account, and the unfeasible ports are erased from the panda dataframes.
+        2 - the closest port to the project site is choosen from the feasbile
+        list of ports.
+    In this case, the ID in the input should be OM_PORT.
+    In both cases, the dimensions of the spare parts should correspond to the biggest dimensions possibly expected.
+    This should be called the first time the logistic module is called.
+"""
+
+install_port = select_port_OM.OM_port(hydrodynamic_outputs, OM_outputs_PORT, ports)  # JUST FOR TESTING!!!
+
+
+
+
+
+logPhase_om = logPhase_om_init(logOp, vessels, equipments, user_inputs, OM_outputs)
+
+if install['status'] == "pending":
+    for index_op, row in OM_outputs.iterrows():
+       log_phase_id = OM_outputs['ID [-]'].ix[index_op]
+       log_phase = logPhase_om[log_phase_id]  # ?!
+
+       # characterize the logistic requirements
+       install['requirement'] = glob_feas(log_phase, log_phase_id,
+                                          user_inputs,
+                                          hydrodynamic_outputs,
+                                          electrical_outputs, MF_outputs)
+
+       # selection of the maritime infrastructure
+       install['eq_select'], log_phase = select_e(install, log_phase)
+       install['ve_select'], log_phase = select_v(install, log_phase)
+
+       # matching requirements for combinations of port/vessel(s)/equipment
+       install['combi_select'], log_phase = compatibility_ve(install,
+                                                             log_phase,
+                                                             install_port['Selected base port for installation'])
+
+       # schedule assessment of the different operation sequence
+       install['schedule'], log_phase = sched(x, install, log_phase,
+                                              log_phase_id, user_inputs,
+                                              hydrodynamic_outputs,
+                                              electrical_outputs,
+                                              MF_outputs)
+
+       # cost assessment of the different operation sequenc
+       install['cost'], log_phase = cost(install, log_phase)
